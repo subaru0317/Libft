8b7fd7108c0541a99899df1c7135289701d71743
subaru0317 <m.kurakura.s@icloud.com>
2023-03-14 16:20:02 +0900

    finish

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: smihata <smihata@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 12:38:15 by smihata           #+#    #+#             */
-/*   Updated: 2023/03/08 16:29:57 by smihata          ###   ########.fr       */
+/*   Updated: 2023/03/14 16:14:40 by smihata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char		*dst_ptr;
-	const char	*src_ptr;
+	unsigned char		*dst_ptr;
+	const unsigned char	*src_ptr;
 
 	if (dst == NULL && src == NULL)
 		return (NULL);
-	dst_ptr = (char *)dst;
-	src_ptr = (char *)src;
+	dst_ptr = (unsigned char *)dst;
+	src_ptr = (unsigned char *)src;
 	if (dst <= src)
 	{
 		while (len--)
