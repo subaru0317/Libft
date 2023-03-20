42ffb7766c82c8b0b6daa69ff4b54af362dfa462
subaru0317 <m.kurakura.s@icloud.com>
2023-03-20 14:14:09 +0900

    fix

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,31 +6,11 @@
 /*   By: smihata <smihata@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 17:03:14 by smihata           #+#    #+#             */
-/*   Updated: 2023/03/20 14:06:16 by smihata          ###   ########.fr       */
+/*   Updated: 2023/03/20 14:09:58 by smihata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*dst;
-	size_t	s_len;
-	size_t	dst_len;
-
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (s_len > start && len != 0)
-		dst_len = ft_min(s_len - start, len);
-	else
-		dst_len = 0;
-	dst = (char *)ft_calloc(dst_len + 1, sizeof(char));
-	if (!dst)
-		return (NULL);
-	ft_strlcpy(dst, s + start, dst_len + 1);
-	return (dst);
-}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
