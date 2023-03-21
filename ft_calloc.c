0e94d37e6bd0f03bb0d8b1a81a21c47f5cd7f441
subaru0317 <m.kurakura.s@icloud.com>
2023-03-21 18:11:07 +0900

    retry

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,17 +6,18 @@
 /*   By: smihata <smihata@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 15:40:02 by smihata           #+#    #+#             */
-/*   Updated: 2023/03/19 16:43:06 by smihata          ###   ########.fr       */
+/*   Updated: 2023/03/20 16:47:22 by smihata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 void	*ft_calloc(size_t number, size_t size)
 {
 	void	*ptr;
 
-	if (size != 0 && number * size / size != number)
+	if (size != 0 && number > SIZE_MAX / size)
 		return (NULL);
 	ptr = (void *)malloc(size * number);
 	if (!ptr)
